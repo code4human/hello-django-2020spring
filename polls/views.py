b3e70697c4108548f1dc8e5d8cb1a4b62b53e656
code4human <tena.kim10@gmail.com>
2020-04-05 03:34:53 +0900

    result 뷰 작성, polls/result.html 생성

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from django.templates import loader
+#from django.templates import loader
+from .models import Question, Choice
 
 # Create your views here.
 def index(request):
@@ -31,8 +32,10 @@ def detail(request, question_id):
     return render(request, 'polls/detail.html', {'question':question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s.")
-    return HttpResponse(response %question_id)
+    #response = "You're looking at the results of question %s.")
+    #return HttpResponse(response %question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question':question})
 
 def vote(request, question_id):
     #return HttpResponse("You're voting on question %s." % question_id)
