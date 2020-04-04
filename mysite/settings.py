834bb351065a1b0b1a8124982cdce7dd394b5905
code4human <tena.kim10@gmail.com>
2020-04-05 00:04:24 +0900

    settings.py 수정, DATABASES 변경, 언어, 시간대 변경

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hello_django', #db-name
+        'USER': 'root', #db-user-name
+        'PASSWORD': 'maria2020', #db-password
+        'HOST':'localhost', #db-address (default localhost 127.0.0.1)
+        'PORT':'3307', #port-number (default 3307)
     }
 }
 
@@ -103,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Seoul'
 
 USE_I18N = True
 
